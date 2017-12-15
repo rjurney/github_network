5291a75ebcb852a1edc3f87b151b7434f129a09e
Russell Jurney <russell.jurney@gmail.com>
2017-12-15 17:31:16 +0300

    Instsall janusgraph script

---
+++ install_janus.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -Lko /tmp/janusgraph-0.2.0-hadoop2.zip https://github.com/JanusGraph/janusgraph/releases/download/v0.2.0/janusgraph-0.2.0-hadoop2.zip
+unzip -d . /tmp/janusgraph-0.2.0-hadoop2.zip
