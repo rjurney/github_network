c03c33cfbcbf20ee9c71fb70ed17a58e86350b45
Russell Jurney <russell.jurney@gmail.com>
2018-01-21 12:11:55 -0800

    In progress querying, adding co_forked edges then running centrality metrics and writing them back to the graph as properties.

--- query.groovy
+++ query.groovy
@@ -3,28 +3,53 @@ graph = JanusGraphFactory.build()\
   .set("storage.backend", "berkeleyje")\
   .set("storage.directory", "data/fork_graph")\
   .set("storage.batch-loading", true)\
-  .set("storage.buffer-size", 10000)\
+  .set("storage.buffer-size", 1000)\
   .open();
 
 // Get a graph traverser
 g = graph.traversal()
 
-// Get a network of users from the network of user -> repos
-repoRepoJson = new groovy.json.JsonBuilder(g.V().hasLabel('repo').as('repo1').in('forked').out('forked').where(neq('repo1')).as('repo2').select('repo1','repo2').by('repoName').dedup()).toPrettyString()
+// Add co-forked edges between nodes
+g.V().
+  hasLabel('repo').
+  store('x').
+  as('repo1').
+  in('forked').
+  out('forked').
+  where(without('x')).
+  as('repo2').
+  addE('co_forked').to('repo1')
 
-/* g.V()\
-  .hasLabel('repo')\
-  .as('repo1')\
-  .in('forked')\
-  .out('forked')\
-  .where(neq('repo1'))\
-  .as('repo2')\
-  .select('repo1','repo2')\
-  .by('repoName')\
-  .dedup(); */
+// Degree centrality of co_forked edges - never returns
+g.V().hasLabel('repo').group().by().by(bothE('co_forked').count())
+g.V().hasLabel('repo').group().by().by(bothE('co_forked').count()).order(local).by(values, decr).limit(local, 20).next()
 
-// Write out to a JSON File
-repoRepoFile = new File("data/repo-repo.jsonl")
-repoRepoFile.write(repoRepoJson)
-repoRepoFile.close()
+// Eigenvector centrality
+g.V().
+  hasLabel('repo').
+  repeat(
+    groupCount('m').
+    by('repoName').
+    out('co_forked').
+    timeLimit(10000)
+  ).
+  times(5).
+  cap('m').
+  order(local).
+  by(values, decr).
+  limit(local, 10).
+  next()
+
+// Write eigenvector centrality to graph property 
+g.V().hasLabel('repo').
+  repeat(
+      groupCount('m').
+        by().
+      out('co_forked').
+      timeLimit(10000)).
+    times(5).
+  cap('m').
+  unfold().as('kv').
+  select(keys).
+    property('eigenvectorCentrality', select('kv').select(values))
 
