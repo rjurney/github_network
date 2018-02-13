afdc49662b09705f56a52627683daa595d4ad8be
Russell Jurney <russell.jurney@gmail.com>
2018-02-13 15:36:10 -0800

    Wrote an implementation of repo-co_forked->repo eigenvector centrality

--- eigenvector.groovy
+++ eigenvector.groovy
@@ -1,9 +1,25 @@
-// Setup our database on top of berkeleydb (for now)
-graph = JanusGraphFactory.build()\
-  .set("storage.backend", "berkeleyje")\
-  .set("storage.directory", "data/fork_graph")\
-  .open();
+// Setup our database on top of cassandra/elasticsearch
+graph = JanusGraphFactory.build().
+  set("storage.backend", "cassandra").
+  set("storage.hostname", "127.0.0.1").
+  set("storage.cassandra.keyspace", "github_graph").
+  set("storage.batch-loading", true).
+  set("storage.buffer-size", 10000).
+  set("index.search.backend", "elasticsearch").
+  open()
 
 // Get a graph traverser
 g = graph.traversal()
 
+// Write eigenvector centrality to graph property 
+g.V().hasLabel('repo').
+  repeat(
+      groupCount('m').
+        by().
+      out('co_forked').
+      timeLimit(1000)).
+    times(5).
+  cap('m').
+  unfold().as('kv').
+  select(keys).
+    property('eigen', select('kv').select(values))
