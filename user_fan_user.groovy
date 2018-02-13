ff14b7025916fcc497aa84f74bb2e89285cab852
Russell Jurney <russell.jurney@gmail.com>
2018-02-13 15:45:25 -0800

    Script to add user-fan->user edges

---
+++ user_fan_user.groovy
@@ -0,0 +1,26 @@
+// Setup our database on top of cassandra/elasticsearch
+graph = JanusGraphFactory.build().
+  set("storage.backend", "cassandra").
+  set("storage.hostname", "127.0.0.1").
+  set("storage.cassandra.keyspace", "github_graph").
+  set("storage.batch-loading", true).
+  set("storage.buffer-size", 10000).
+  set("index.search.backend", "elasticsearch").
+  open()
+
+g = graph.traversal()
+
+// Create the user-forks->user edges, printing a counter/committing transactions every 1,000 records
+g.V().
+  hasLabel('user').
+  as('user1').
+  out('owned').
+  in('forked').
+  as('user2').
+  addEdge('forked').
+  to('user1').
+  choose(
+      filter{it->count+=1; count%1000 == 0},
+      __.map{it->println(count); g.tx().commit(); it.get()},
+      __.identity()
+  )
