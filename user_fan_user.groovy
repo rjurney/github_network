8579ff8068df06ca7bbf39e8fa126066f444d4d9
Russell Jurney <russell.jurney@gmail.com>
2018-02-16 22:14:21 -0800

    Hop across user-forked->repo<-owned-user edges to form user-fan->user edges.

--- user_fan_user.groovy
+++ user_fan_user.groovy
@@ -11,16 +11,28 @@ graph = JanusGraphFactory.build().
 g = graph.traversal()
 
 // Create the user-forks->user edges, printing a counter/committing transactions every 1,000 records
+count = 0
+
 g.V().
   hasLabel('user').
   as('user1').
   out('owned').
   in('forked').
   as('user2').
-  addEdge('forked').
+  addE('fan').
   to('user1').
   choose(
       filter{it->count+=1; count%1000 == 0},
       __.map{it->println(count); g.tx().commit(); it.get()},
       __.identity()
-  )
+  ).
+  iterate()
+
+println(count)
+
+// Verify the count is correct
+fanCount = g.E().
+  hasLabel('fan').
+  count().
+  next()
+assert(fanCount == 9451177)
