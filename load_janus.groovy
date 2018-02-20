dc5185db8698f30ca2315ade153c76415bccde3b
Russell Jurney <russell.jurney@gmail.com>
2018-02-19 17:30:40 -0800

    New counts to verify, now formatting counts when printing them, changed path to relative to home directory

--- load_janus.groovy
+++ load_janus.groovy
@@ -1,3 +1,5 @@
+import java.text.NumberFormat;
+
 // Setup our database on top of cassandra/elasticsearch
 graph = JanusGraphFactory.build().
   set("storage.backend", "cassandra").
@@ -11,44 +13,56 @@ graph = JanusGraphFactory.build().
 // Get a graph traverser
 g = graph.traversal()
 
-// Setup JSON Reading of MongoDB mongodump data
+// Setup JSON Reading of data
 jsonSlurper = new JsonSlurper()
 
 // Add user nodes to graph
-// usersFilename = "../github_network/data/users.jsonl"
-usersFilename = "../data/users.jsonl"
+usersFilename = "../github_network/data/users.jsonl"
+// usersFilename = "../data/users.jsonl"
 usersReader = new BufferedReader(new FileReader(usersFilename));
 
+i = 0
 while((json = usersReader.readLine()) != null)
 {
   document = jsonSlurper.parseText(json)
 
   v = graph.addVertex('user')
+  v.property("name", document.user)
   v.property("userName", document.user)
-  graph.tx().commit()
 
-  print("U")
+  if(i % 1000 == 0) {
+    graph.tx().commit()
+    str = NumberFormat.getIntegerInstance().format(i)
+    println(str + "U")
+  }
+  i++
 }
 
 // Add repo nodes to graph
-// reposFilename = "../github_network/data/repos.jsonl"
-reposFilename = "../data/repos.jsonl"
+reposFilename = "../github_network/data/repos.jsonl"
+// reposFilename = "../data/repos.jsonl"
 reposReader = new BufferedReader(new FileReader(reposFilename))
 
+i = 0
 while((json = reposReader.readLine()) != null)
 {
   document = jsonSlurper.parseText(json)
 
   v = graph.addVertex('repo')
+  v.property("name", document.repo)
   v.property("repoName", document.repo)
-  graph.tx().commit()
 
-  print("R")
+  if(i % 1000 == 0) {
+    graph.tx().commit()
+    str = NumberFormat.getIntegerInstance().format(i)
+    println(str + "R")
+  }
+  i++
 }
 
 // Create forked edges between users and repos
-// forkEdgesFilename = "../github_network/data/users_forked_repos.jsonl"
-forkEdgesFilename = "../data/users_forked_repos.jsonl"
+forkEdgesFilename = "../github_network/data/users_forked_repos.jsonl"
+// forkEdgesFilename = "../data/users_forked_repos.jsonl"
 forkEdgesReader = new BufferedReader(new FileReader(forkEdgesFilename));
 
 i = 0
@@ -61,17 +75,18 @@ while((json = forkEdgesReader.readLine()) != null)
   repo = g.V().has('repoName', document.repo).next()
 
   user.addEdge("forked", repo)
-  graph.tx().commit()
 
-  if((i % 1000) == 0) {
-    print("On edge number: " + i + "\n")
+  if(i % 1000 == 0) {
+    graph.tx().commit()
+    str = NumberFormat.getIntegerInstance().format(i)
+    println(str + "F")
   }
-  i = i + 1
+  i++
 }
 
 // Create starred edges between users and repos
-// starEdgesFilename = "../github_network/data/users_starred_repos.jsonl"
-starEdgesFilename = "../data/users_starred_repos.jsonl"
+starEdgesFilename = "../github_network/data/users_starred_repos.jsonl"
+// starEdgesFilename = "../data/users_starred_repos.jsonl"
 starEdgesReader = new BufferedReader(new FileReader(starEdgesFilename));
 
 i = 0
@@ -84,17 +99,18 @@ while((json = starEdgesReader.readLine()) != null)
   repo = g.V().has('repoName', document.repo).next()
 
   user.addEdge("starred", repo)
-  graph.tx().commit()
 
-  if((i % 1000) == 0) {
-    print("On edge number: " + i + "\n")
+  if(i % 1000 == 0) {
+    graph.tx().commit()
+    str = NumberFormat.getIntegerInstance().format(i)
+    println(str + "S")
   }
-  i = i + 1
+  i++
 }
 
 // Create fan edges between users and other users
-// ownEdgesFilename = "../github_network/data/users_own_repos.jsonl"
-ownEdgesFilename = "../data/users_own_repos.jsonl"
+ownEdgesFilename = "../github_network/data/users_owned_repos.jsonl"
+// ownEdgesFilename = "../data/users_owned_repos.jsonl"
 ownEdgesReader = new BufferedReader(new FileReader(ownEdgesFilename));
 
 i = 0
@@ -103,33 +119,41 @@ while((json = ownEdgesReader.readLine()) != null)
   document = jsonSlurper.parseText(json)
 
   // Fetch the user and repo
-  user = g.V().has('userName', document.user).next()
+  user = g.V().has('userName', document.owner).next()
   repo = g.V().has('repoName', document.repo).next()
 
   user.addEdge("owned", repo)
-  graph.tx().commit()
 
-  if((i % 1000) == 0) {
-    print("On edge number: " + i + "\n")
+  if(i % 1000 == 0) {
+    graph.tx().commit()
+    str = NumberFormat.getIntegerInstance().format(i)
+    println(str + "O")
   }
-  i = i + 1
+  i++
 }
 
 // Setup sessions so we can remember variables
 :remote connect tinkerpop.server conf/remote.yaml session
 
 userCount = g.V().hasLabel('user').count().next()
-print(userCount)
-assert(userCount == 4067599)
+println(userCount)
+assert(userCount == 4402402)
 
 repoCount = g.V().hasLabel('repo').count().next()
-print(repoCount)
+println(repoCount)
 assert(repoCount == 4071996)
 
 forkedCount = g.E().hasLabel('forked').count().next()
-print(forkedCount)
+println(forkedCount)
 assert(forkedCount == 11366334)
 
 starredCount = g.E().hasLabel('starred').count().next()
-print(starredCount)
-assert(starredCount == 31870088)
+println(starredCount)
+assert(starredCount == 31870051)
+
+ownedCount = g.E().hasLabel('owned').count().next()
+println(ownedCount)
+assert(ownedCount == 2263684)
+
+// Close graph
+graph.close()
