9d6c95403f100b46098ef12f1971a52bed70108a
Russell Jurney <russell.jurney@gmail.com>
2018-02-20 11:44:49 -0800

    Final working script for user-fan centrality

--- user_fan_centrality.groovy
+++ user_fan_centrality.groovy
@@ -11,34 +11,81 @@ graph = JanusGraphFactory.build().
 // Get a graph traverser
 g = graph.traversal()
 
+// Inspect a top 20 degree centrality of fans
+g.V().
+  hasLabel('user').
+  group().
+  by('userName').
+  as('owner').
+  by(
+    inE('fan').
+    count()
+  ).
+  as('degree').
+  select('owner','degree').
+  order().
+  by(
+    select('degree'),
+    decr
+  ).
+  limit(20)
+
+// Inspect a top 20 eigenvector centrality of fans
+g.V().
+  hasLabel('user').
+  repeat(
+    groupCount('m').
+    by('userName').
+    out('fan').
+    timeLimit(30000)
+  ).
+  times(5).
+  cap('m').
+  order(local).
+  by(values, decr).
+  limit(local, 20).
+  next()
+
 // Compute degree centrality of users via user-fan->user degree
 // and write it to the 'degree' node property
+count = 0
+
 g.V().
   hasLabel('user').
   group().
   by().
   by(
-    inE().
+    inE('fan').
     count()
   ).
   unfold().
   as('kv').
   select(keys).
   property(
-    'degree', 
+    'fan_degree', 
     select('kv').
-    select(values)
-  )
+      select(values)
+  ).
+  choose(
+    filter{it->count+=1; count%1000 == 0},
+    __.map{it->println(count); g.tx().commit(); it.get()},
+    __.identity()
+  ).
+  iterate()
+
+println(count)
 
 // Calculate eigenvector centrality on the user-fan->user edges
 // and write it to the 'eigen' node property
+count = 0
+
 g.V().
   hasLabel('user').
   repeat(
     groupCount('m').
     by().
     out('fan').
-    timeLimit(10000)
+    timeLimit(30000)
   ).
   times(5).
   cap('m').
@@ -46,8 +93,15 @@ g.V().
   as('kv').
   select(keys).
   property(
-    'eigen',
+    'fan_eigen',
     select('kv').
     select(values)
-  )
+  ).
+  choose(
+    filter{it->count+=1; count%1000 == 0},
+    __.map{it->println(count); g.tx().commit(); it.get()},
+    __.identity()
+  ).
+  iterate()
 
+println(count)
