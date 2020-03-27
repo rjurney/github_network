22a5b608370aa12b8510791135163faed2bb0fa7
Russell Jurney <russell.jurney@gmail.com>
2020-03-26 19:30:25 -0700

    Updated download script for 2020

--- download.sh
+++ download.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir data
-cd data
+mkdir -p data/github
+cd data/github
 wget http://data.githubarchive.org/2017-{01..12}-{01..31}-{0..23}.json.gz
-cd ..
+wget http://data.githubarchive.org/2018-{01..12}-{01..31}-{0..23}.json.gz
+wget http://data.githubarchive.org/2019-{01..12}-{01..31}-{0..23}.json.gz
+wget http://data.githubarchive.org/2020-{01..12}-{01..31}-{0..23}.json.gz
+cd ../..
